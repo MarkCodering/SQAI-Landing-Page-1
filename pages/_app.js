6bf7a9b32ccb3ef0ca472403259700850dcc1c6b
MarkCodering <mark.chen.sstm@gmail.com>
2023-04-08 11:07:06 +0800

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
 import "../styles/globals.css";
 import Layout from "./components/Layout";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme="dark">
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <Analytics />
     </ChakraProvider>
   );
 }
